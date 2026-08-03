fdd4fb3364d89c11caadd236ad51a2ad80827118
starckyang <110225563+starckyang@users.noreply.github.com>
2026-08-03 12:30:23 +0800

    Add: jump-game - submission-5

---
+++ Data Structures & Algorithms/jump-game/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # method2: greedy
+        i=0
+        visited=[False]*len(nums)
+        while i<len(nums):
+            jump=nums[i]
+            if (i+jump+1) >= len(nums):
+                return True
+            if jump==0:
+                return False
+            cur_max=0
+            next_pos=i
+            for step in range(1, jump+1):
+                if visited[step]:
+                    continue
+                reach=step+nums[i+step]
+                if reach>cur_max:
+                    cur_max=max(cur_max, reach)
+                    next_pos=i+step
+            i=next_pos
+            
